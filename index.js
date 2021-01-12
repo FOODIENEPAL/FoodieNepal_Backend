c711883d9553102d81648d8c2eb2de1ae27d67e6
Sujan Majkoti <sujansoftwarica2020@gmail.com>
2021-01-12 20:23:19 +0545

    Added food and food category

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
 const userRouter = require('./routes/user');
 const adminRouter = require('./routes/admin');
+const foodRouter = require('./routes/food');
+const uploadRouter = require('./routes/uploads');
+const resturantRouter = require('./routers/resturant');
 const dotenv = require('dotenv').config();
 
 
